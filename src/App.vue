911c2e60e76f1463ef03f595015794f39ecffa4e
Roman <romanranniew@gmail.com>
2021-07-23 16:35:37 +0200

    Init canvas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <game />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Game from './components/game'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        Game
+    }
 }
 </script>
 
 <style>
+html {
+    height: 100%;
+}
+body {
+    margin: 0;
+    height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background: #40474a;
 }
 </style>
---
+++ src/components/game.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="game">
+        <div class="canvas-wrap" id="canvas"></div>
+    </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+import { onMounted } from '@vue/runtime-core'
+export default {
+    setup() {
+        let context
+        const drawPoint = function(position, size = 3, color = '#80baa0') {
+            context.fillStyle = color
+            context.beginPath()
+            context.arc(position.x, position.y, size, 0, 2 * Math.PI, true)
+            context.fill()
+        }
+        onMounted(() => {
+            const canvasWidth = 800
+            const canvasHeight = 800
+            const canvas = d3
+                .select('#canvas')
+                .append('canvas')
+                .attr('width', canvasWidth)
+                .attr('height', canvasHeight)
+            context = canvas.node().getContext('2d')
+            context.clearRect(0, 0, canvasWidth, canvasHeight)
+            drawPoint({ x: 100, y: 600 })
+        })
+    }
+}
+</script>
+
+<style>
+.canvas-wrap canvas {
+    border: 5px solid #50aa90;
+    outline: 5px solid #80baa0;
+}
+</style>
